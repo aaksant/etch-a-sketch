5173a90ef5408050b84f30c1fc2e816d6f530196
aaksant <aaksangit@gmail.com>
2023-11-30 10:03:43 +0700

    Add clear grid feature

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 const dimensionInput = document.querySelector('#dimension');
 const createGridButton = document.querySelector('.create-grid-button');
+const resetButton = document.querySelector('.reset-button');
 
 const board = document.querySelector('.board');
-const gridCreationInfo = document.querySelector('.grid-creation-info');
+const gridInfo = document.querySelector('.grid-info');
 
 function createGrid() {
   let dimension = parseInt(dimensionInput.value);
 
   if (dimension < 2 || dimension > 100) {
-    gridCreationInfo.textContent = '2 <= grid <= 100';
+    gridInfo.textContent = '2 <= grid <= 100';
     console.log(typeof dimension);
   } else {
-    gridContainer.style.setProperty('--dimension', dimension);
+    board.style.setProperty('--dimension', dimension);
 
     for (let i = 0; i < dimension ** 2; i++) {
       let grid = document.createElement('div');
       grid.style.backgroundColor = 'white';
-      gridContainer.appendChild(grid);
+      board.appendChild(grid);
     }
 
-    gridCreationInfo.textContent = 'Grid created!';
+    gridInfo.textContent = 'Grid created';
   }
 }
 
+function reset() {
+  let grids = board.querySelectorAll('div');
+  grids.forEach((grid) => grid.remove());
+
+  gridInfo.textContent = 'Grid cleared'
+}
+
 createGridButton.addEventListener('click', createGrid);
+resetButton.addEventListener('click', reset);
