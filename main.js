864381116f1d102c23863b8f89fe512232fc02f4
aaksant <aaksangit@gmail.com>
2023-12-02 09:27:27 +0700

    Add hover to change color effect

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ const resetButton = document.querySelector('.reset-button');
 const board = document.querySelector('.board');
 const gridInfo = document.querySelector('.grid-info');
 
+const mainColor = 'black';
+
 function createGrid() {
   let dimension = parseInt(dimensionInput.value);
 
   if (dimension < 2 || dimension > 100) {
     gridInfo.textContent = '2 <= grid <= 100';
-    console.log(typeof dimension);
   } else {
     board.style.setProperty('--dimension', dimension);
 
@@ -18,6 +19,10 @@ function createGrid() {
       let grid = document.createElement('div');
       grid.style.backgroundColor = 'white';
       board.appendChild(grid);
+
+      grid.addEventListener('mouseover', () => {
+        grid.style.backgroundColor = mainColor;
+      });
     }
 
     gridInfo.textContent = 'Grid created';
@@ -28,6 +33,7 @@ function reset() {
   let grids = board.querySelectorAll('div');
   grids.forEach((grid) => grid.remove());
 
+  dimensionInput.value = '';
   gridInfo.textContent = 'Grid cleared'
 }
 
